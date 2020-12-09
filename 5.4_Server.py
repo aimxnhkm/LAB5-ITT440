d1bede840d5fb7ca68e5d3fad8c822da2dd00bc9
aimxn <aimxn@lab-Server>
2020-12-09 15:15:40 +0000

    File Storage Application

--- 5.4_Server.py
+++ 5.4_Server.py
@@ -1,11 +1,11 @@
 import socket
 import sys
 
-print("[+]Welcome to FTP File Storage")
+print("[+]Welcome to FTP Application")
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 print("[+]Socket succesfully created")
 
-PORT = 8005
+PORT = 8888
 
 s.bind(('', PORT))
 print("[+]Socket binded to " + str(PORT))
@@ -15,11 +15,12 @@ print('[+]Waiting for a connection...')
 
 while True:
 	conn, addr = s.accept()
+	print("[+]Connection is accepted from the Client")
 
 	filename = conn.recv(1024)
 	file = open(filename, "wb")
 
-	msg ="[+]Hi Client[IP address: " + addr[0] + "], \n**Welcome to Server** \n Thank You!\n"
+	msg ="[+]Hi Client[IP address: " + addr[0] + "], \n **Welcome to Server** \n Thank You! \n"
 	conn.send(msg.encode("utf-8"))
 
 	RecvData = conn.recv(1024)
@@ -28,9 +29,9 @@ while True:
 		RecvData = conn.recv(1024)
 
 	file.close()
-	print("[+]File has been copied successfully")
+	print("[+]File has been copied and stored successfully")
 
 	conn.close()
-	print("[+]Close the connection")
+	print("[+]Server close the connection \n")
 
 	break
